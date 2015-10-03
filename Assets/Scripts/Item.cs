37f08767d79b5db57c8d023b0edf50c7080af11b
Alex Bronner <acb8570@g.rit.edu>
2015-10-03 15:47:57 -0400

    Added manager and item prefabs with scripts

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour
+{
+    public int xPos;
+    public int zPos;
+
+	// Use this for initialization
+    void Update()
+    {
+        transform.position = new Vector3(xPos, .5f, zPos);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;   //To use list
+
+public class Manager : MonoBehaviour
+{
+    List<GameObject> items;
+
+	// Use this for initialization
+	void Start ()
+    {
+        items = new List<GameObject>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (items.Count < 1)
+        {
+            GameObject newItem = (GameObject)Instantiate(Resources.Load("Item"));
+            newItem.GetComponent<Item>().xPos = Random.Range(0, 5);
+            newItem.GetComponent<Item>().zPos = Random.Range(0, 5);
+            items.Add(newItem);
+        }
+    }
+}
