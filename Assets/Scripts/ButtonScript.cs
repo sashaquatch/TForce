bff781e960c3881626edc8d038fbfa2d7432db9a
sashaquatch <sasha.gonye.13@gmail.com>
2015-10-03 13:56:19 -0700

    Added Menus
    I added a main menu that links to the game scene. If the button doesn't
    work go to file build settings and drop the main menu in then the game
    scene.

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void LoadFirstLevel()
+    {
+        //load the first level
+        Application.LoadLevel(1);
+    }
+
+    public void LoadMainMenu()
+    {
+        //load the main menu
+        Application.LoadLevel(0);
+    }
+
+    public void LoadScore()
+    {
+        //load the score screen
+        Application.LoadLevel(2);
+    }
+}
---
+++ Assets/Scripts/GridDisp.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridDisp : MonoBehaviour {
+
+    //display this object on the grid
+    public GameObject gridDisp;
+
+	// Use this for initialization
+	void Start () {
+	
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+
+
+
+	}
+}
