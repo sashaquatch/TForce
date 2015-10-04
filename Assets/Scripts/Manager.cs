4ad4f858dbdc8b1e96cdd710c7c60cd6650e5e4b
Alexander Bronner <acb8570@g.rit.edu>
2015-10-04 04:26:50 -0400

    Modified manager so that items probably don't spawn in occupied locations.

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -4,12 +4,16 @@ using System.Collections.Generic;   //To use list
 
 public class Manager : MonoBehaviour
 {
-    List<GameObject> items;
+    private List<GameObject> items;
+    private List<GameObject> snakeParts;    //Snake parts for spawning comparison
+    private List<GameObject> snakeHeads;    //Snake heads for spawning comparison
 
 	// Use this for initialization
 	void Start ()
     {
         items = new List<GameObject>();
+        snakeParts = new List<GameObject>();
+        snakeHeads = new List<GameObject>();
         generateItems();
     }
 	
@@ -20,17 +24,91 @@ public class Manager : MonoBehaviour
         generateItems();
     }
 
+    //Generate new items
     void generateItems()
     {
+        //If there less than 3 items, fill new spawning comparison lists.
+        if (items.Count < 3)
+        {
+            snakeParts = new List<GameObject>();
+            snakeHeads = new List<GameObject>();
+            GameObject[] allObjects = (GameObject[])FindObjectsOfType(typeof(GameObject));
+            for(int i = 0; i < allObjects.Length; i++)
+            {
+                if(allObjects[i].GetComponent<SnekPartMove>() != null)
+                {
+                    snakeParts.Add(allObjects[i]);
+                }
+                if(allObjects[i].GetComponent<SnekHead>() != null)
+                {
+                    snakeHeads.Add(allObjects[i]);
+                }
+            }
+        }
+
+        //While there are less than 3 items, generate a new item
         while (items.Count < 3)
         {
-            GameObject newItem = (GameObject)Instantiate(Resources.Load("Item"));
-            newItem.GetComponent<Item>().xPos = Random.Range(0, 10);
-            newItem.GetComponent<Item>().zPos = Random.Range(0, 10);
-            items.Add(newItem);
+            GameObject newItem = (GameObject)Instantiate(Resources.Load("Item"));   //New item to spawn
+            bool occupied = true;                                                   //True if spawn location is occupied
+
+            //Loop spawn occupation check
+            while (occupied)
+            {
+                occupied = false;
+
+                //New location
+                newItem.GetComponent<Item>().xPos = Random.Range(0, 10);
+                newItem.GetComponent<Item>().zPos = Random.Range(0, 10);
+
+                //Check if location is occupied by snake parts.
+                for(int i = 0; i < snakeParts.Count; i++)
+                {
+                    if(
+                        newItem.GetComponent<Item>().xPos == snakeParts[i].GetComponent<SnekPartMove>().xPos &&
+                        newItem.GetComponent<Item>().zPos == snakeParts[i].GetComponent<SnekPartMove>().zPos)
+                    {
+                        occupied = true;
+                        break;
+                    }
+                }
+
+                //Check if location is occupied by snake heads.
+                if(occupied == false)
+                {
+                    for (int i = 0; i < snakeHeads.Count; i++)
+                    {
+                        if (
+                            newItem.GetComponent<Item>().xPos == snakeHeads[i].GetComponent<SnekHead>().xPos &&
+                            newItem.GetComponent<Item>().zPos == snakeHeads[i].GetComponent<SnekHead>().zPos)
+                        {
+                            occupied = true;
+                            break;
+                        }
+                    }
+                }
+
+                //Check if location is occupied by other items.
+                if(occupied == false)
+                {
+                    for(int i = 0; i > items.Count; i++)
+                    {
+                        if (
+                            newItem.GetComponent<Item>().xPos == items[i].GetComponent<Item>().xPos &&
+                            newItem.GetComponent<Item>().zPos == items[i].GetComponent<Item>().zPos)
+                        {
+                            occupied = true;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            items.Add(newItem); //Add new item
         }
     }
 
+    //Remove all null-missing items.
     void cleanItems()
     {
         for (int i = 0; i < items.Count; i++)
