3d9615f9f844e347734432b24f221ad4a8f2b7f1
Alex Bronner <acb8570@g.rit.edu>
2015-10-18 16:06:54 -0400

    Added Menu instructions and controls!

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject instructions;
+    public GameObject controls;
+    
+	public void EnableDisableInstructions()
+    {
+        if (instructions.activeSelf == true)
+        {
+            instructions.SetActive(false);
+        }
+        else
+        {
+            instructions.SetActive(true);
+            controls.SetActive(false);
+        }
+    }
+
+    public void EnableDisableControls()
+    {
+        if (controls.activeSelf == true)
+        {
+            controls.SetActive(false);
+        }
+        else
+        {
+            controls.SetActive(true);
+            instructions.SetActive(false);
+        }
+    }
+}
